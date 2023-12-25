402eb08d5f16eb281d18fc8843bdead82ac3d9c8
Harold-coder <hjc2154@columbia.edu>
2023-12-25 18:22:49 +0100

    Made the Flask Backend for phase 1

---
+++ main.py
@@ -0,0 +1,233 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+from config import SECRET_KEY
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:password@unilate-test.cl020ce0qv5c.eu-north-1.rds.amazonaws.com/unilate'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Doctor(db.Model):
+    __tablename__ = 'Doctors'
+    
+    DoctorID = db.Column(db.Integer, primary_key=True)  
+    Name = db.Column(db.String(255), nullable=False) 
+    Specialty = db.Column(db.String(255))  
+    City = db.Column(db.String(255))  
+    Email = db.Column(db.String(255), nullable=False, unique=True)  
+    PhoneNumber = db.Column(db.String(20))  
+    HospitalName = db.Column(db.String(255))  
+    PasswordHash = db.Column(db.String(255))  
+
+    def set_password(self, password):
+        self.PasswordHash = generate_password_hash(password)  
+
+    def check_password(self, password):
+        return check_password_hash(self.PasswordHash, password)  
+
+class Delay(db.Model):
+    __tablename__ = 'Delays'
+    
+    DelayID = db.Column(db.Integer, primary_key=True)  
+    DoctorID = db.Column(db.Integer, db.ForeignKey('Doctors.DoctorID'), nullable=False)  
+    DelayDuration = db.Column(db.Integer, nullable=False)  
+    StartTimestamp = db.Column(db.DateTime, nullable=False)  
+    EndTimestamp = db.Column(db.DateTime, nullable=False)  
+    AnnouncementTimestamp = db.Column(db.DateTime, nullable=False) 
+
+
+# Wrap db.create_all in an application context
+with app.app_context():
+    db.create_all()
+
+
+# Utility function for verifying JWT tokens
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-tokens' in request.headers:
+            token = request.headers['x-access-tokens']
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = Doctor.query.filter_by(DoctorID=data['doctor_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+# Registration endpoint
+@app.route('/doctors/register', methods=['POST'])
+def register_doctor():
+    data = request.get_json()
+    doctor = Doctor(
+        Name=data['name'],
+        Specialty=data['specialty'],
+        City=data['city'],
+        Email=data['email'],
+        PhoneNumber=data['phone_number'],
+        HospitalName=data['hospital_name']
+    )
+    doctor.set_password(data['password'])
+    db.session.add(doctor)
+    try:
+        db.session.commit()
+        return jsonify({'message': 'New doctor registered'}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 500
+
+# Login endpoint
+@app.route('/doctors/login', methods=['POST'])
+def login_doctor():
+    auth = request.json
+
+    if not auth or not auth.get('email') or not auth.get('password'):
+        return jsonify({'message': 'Could not verify', 'WWW-Authenticate': 'Basic realm="Login required!"'}), 401
+
+    doctor = Doctor.query.filter_by(Email=auth.get('email')).first()
+
+    if not doctor:
+        return jsonify({'message': 'Doctor not found', 'WWW-Authenticate': 'Basic realm="Login required!"'}), 401
+
+    if doctor.check_password(auth.get('password')):
+        token = jwt.encode({'doctor_id': doctor.DoctorID, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, app.config['SECRET_KEY'], algorithm="HS256")
+        return jsonify({'token': token})
+
+    return jsonify({'message': 'Could not verify', 'WWW-Authenticate': 'Basic realm="Login required!"'}), 401
+
+# Retrieve doctor info
+@app.route('/doctors/<int:doctor_id>', methods=['GET'])
+def get_doctor(doctor_id):
+    doctor = Doctor.query.filter_by(DoctorID=doctor_id).first()
+
+    if not doctor:
+        return jsonify({'message': 'Doctor not found'}), 404
+
+    doctor_data = {
+        'id': doctor.DoctorID,
+        'name': doctor.Name,
+        'specialty': doctor.Specialty,
+        'city': doctor.City,
+        'email': doctor.Email,
+        'phone_number': doctor.PhoneNumber,
+        'hospital_name': doctor.HospitalName
+    }
+
+    return jsonify({'doctor': doctor_data}), 200
+
+@app.route('/doctors/<int:doctor_id>', methods=['PUT'])
+@token_required
+def update_doctor(current_user, doctor_id):
+    if current_user.DoctorID != doctor_id:
+        return jsonify({'message': 'Permission denied'}), 403
+    
+    data = request.get_json()
+    doctor = Doctor.query.filter_by(DoctorID=doctor_id).first()
+
+    if not doctor:
+        return jsonify({'message': 'Doctor not found'}), 404
+
+    doctor.Name = data.get('name', doctor.Name)
+    doctor.Specialty = data.get('specialty', doctor.Specialty)
+    doctor.City = data.get('city', doctor.City)
+    doctor.Email = data.get('email', doctor.Email)
+    doctor.PhoneNumber = data.get('phone_number', doctor.PhoneNumber)
+    doctor.HospitalName = data.get('hospital_name', doctor.HospitalName)
+
+    db.session.commit()
+
+    return jsonify({'message': 'Doctor profile updated successfully'}), 200
+
+@app.route('/delays/<int:doctor_id>', methods=['PUT'])
+@token_required
+def update_delay(current_user, doctor_id):
+    # Ensure the current user is the doctor whose delay is being updated
+    if current_user.DoctorID != doctor_id:
+        return jsonify({'message': 'Permission denied'}), 403
+
+    # Get the delay update data from the request
+    data = request.get_json()
+    
+    # Find the delay entry for the doctor
+    delay = Delay.query.filter_by(DoctorID=doctor_id).first()
+
+    if not delay:
+        return jsonify({'message': 'Delay entry not found for the doctor'}), 404
+
+    # Update the delay entry with the new information
+    delay.DelayDuration = data.get('delay_duration', delay.DelayDuration)
+    delay.StartTimestamp = data.get('start_timestamp', delay.StartTimestamp)
+    delay.EndTimestamp = data.get('end_timestamp', delay.EndTimestamp)
+    delay.AnnouncementTimestamp = data.get('announcement_timestamp', delay.AnnouncementTimestamp)
+    
+    # Commit the changes to the database
+    db.session.commit()
+    
+    return jsonify({'message': 'Delay updated successfully'}), 200
+
+@app.route('/delays/<int:doctor_id>', methods=['GET'])
+def get_current_delay(doctor_id):
+    # Query the database for the delay associated with the doctor_id
+    delay = Delay.query.filter_by(DoctorID=doctor_id).first()
+
+    # If no delay entry is found for the doctor, return a 404 error
+    if not delay:
+        return jsonify({'message': 'No delay entry found for this doctor'}), 404
+
+    # Prepare the delay information to return
+    current_delay = {
+        'doctor_id': delay.DoctorID,
+        'delay_duration': delay.DelayDuration,
+        'start_timestamp': delay.StartTimestamp.isoformat() + 'Z',  # Assuming UTC timezone
+        'end_timestamp': delay.EndTimestamp.isoformat() + 'Z',  # Assuming UTC timezone
+        'announcement_timestamp': delay.AnnouncementTimestamp.isoformat() + 'Z'  # Assuming UTC timezone
+    }
+
+    # Return the delay information in the response
+    return jsonify(current_delay), 200
+
+
+@app.route('/doctors', methods=['GET'])
+def search_doctors():
+    name = request.args.get('name')
+    city = request.args.get('city')
+    specialty = request.args.get('specialty')
+
+    query = Doctor.query
+
+    if name:
+        query = query.filter(Doctor.Name.ilike(f'%{name}%'))
+    if city:
+        query = query.filter(Doctor.City.ilike(f'%{city}%'))
+    if specialty:
+        query = query.filter(Doctor.Specialty.ilike(f'%{specialty}%'))
+
+    doctors = query.all()
+    doctors_data = [{
+        'id': doctor.DoctorID,
+        'name': doctor.Name,
+        'specialty': doctor.Specialty,
+        'city': doctor.City,
+        'email': doctor.Email,
+        'phone_number': doctor.PhoneNumber,
+        'hospital_name': doctor.HospitalName
+    } for doctor in doctors]
+
+    return jsonify({'doctors': doctors_data})
+
+
+# Health Check endpoint
+@app.route('/', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'healthy'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8012, use_reloader=False)
---
+++ test.py
@@ -0,0 +1,115 @@
+import requests
+from datetime import datetime, timedelta
+
+def test_register_doctor():
+    url = 'http://10.50.2.238:8012/doctors/register'
+    data = {
+        'name': 'Jane Smith',
+        'specialty': 'Pediatrics',
+        'city': 'Springfield',
+        'email': 'janesmith@example.com',  # Ensure this email is unique and not already registered
+        'phone_number': '9876543210',
+        'hospital_name': 'Springfield General Hospital',
+        'password': 'test123'  # Provide a new password for registration
+    }
+
+    response = requests.post(url, json=data)
+    print(response.json())
+
+def test_login_doctor(): 
+    url = 'http://10.50.2.238:8012/doctors/login'
+    data = {
+        'email': 'janesmith@example.com',  # Use the email address you registered the doctor with
+        'password': 'test123'  # Use the password you set for the doctor
+    }
+
+    response = requests.post(url, json=data)
+    print(response.json())
+
+def test_get_doctor():
+    doctor_id = 6
+    # Replace '1' with the actual DoctorID you want to retrieve
+    public_info_url = f'http://10.50.2.238:8012/doctors/{doctor_id}'
+
+    # Send a GET request
+    response = requests.get(public_info_url)
+
+    # Print out the response
+    print(response.json())
+
+def test_update_doctor():
+    # First, log in to get the token
+    login_url = 'http://10.50.2.238:8012/doctors/login'
+    login_data = {
+        'email': 'janesmith@example.com',
+        'password': 'test123'
+    }
+    login_response = requests.post(login_url, json=login_data)
+    token = login_response.json().get('token')
+
+    doctor_id = 6
+
+    update_data = {
+        'name': 'Jane Smith Updated',
+        'specialty': 'Pediatrics',
+        'city': 'New Springfield',
+        # You can add other fields to update as needed
+    }
+    
+    # Then, attempt to update the doctor's profile
+    update_url = f'http://10.50.2.238:8012/doctors/{doctor_id}'
+    headers = {'x-access-tokens': token}
+    
+    response = requests.put(update_url, headers=headers, json=update_data)
+    print(response.json())
+
+
+def test_update_delay():
+    # Log in to get the token
+    login_url = 'http://10.50.2.238:8012/doctors/login'
+    login_data = {
+        'email': 'janesmith@example.com',
+        'password': 'test123'
+    }
+    login_response = requests.post(login_url, json=login_data)
+    token = login_response.json().get('token')
+
+    doctor_id = 6
+    new_delay_duration = 0
+    
+    # Update the delay
+    update_url = f'http://10.50.2.238:8012/delays/{doctor_id}'
+    headers = {'x-access-tokens': token}
+    update_data = {
+        'delay_duration': new_delay_duration,
+        'start_timestamp': datetime.utcnow().isoformat() + 'Z',  # Convert to ISO 8601 format
+        'end_timestamp': (datetime.utcnow() + timedelta(minutes=120)).isoformat() + 'Z',  # Convert to ISO 8601 format
+        'announcement_timestamp': datetime.utcnow().isoformat() + 'Z'  # Convert to ISO 8601 format
+    }
+    
+    response = requests.put(update_url, headers=headers, json=update_data)
+    print(response.json())
+
+
+def test_get_current_delay():
+    doctor_id = 6
+    url = f'http://10.50.2.238:8012/delays/{doctor_id}'
+
+    # Send a GET request to the endpoint
+    response = requests.get(url)
+
+    # Print out the response
+    print(response.json())
+
+
+def test_search_doctors():
+    url = 'http://10.50.2.238:8012/doctors'
+    params = {
+        'name': '',
+        'city': 'Sydney',
+        'specialty': ''
+    }
+
+    response = requests.get(url, params=params)
+    print(response.json())
+
